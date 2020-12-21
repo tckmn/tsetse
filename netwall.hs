96bb52e28f4128af7c81617c66c3208c737d5978
Andy Tockman <andy@tck.mn>
2020-12-21 16:50:58 -0600

    first working prototype

---
+++ netwall.hs
@@ -0,0 +1,278 @@
+#!/usr/bin/env runhaskell
+
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE TupleSections #-}
+
+import Prelude hiding (log)
+
+import Control.Concurrent
+import Control.Exception (finally)
+import Control.Monad
+import Control.Monad.Trans
+import Control.Monad.Trans.Maybe
+import Data.Char (isUpper, isAscii)
+import Data.Functor
+import Data.List
+import Data.Map (Map)
+import Data.Maybe
+import Data.Text (Text)
+import Data.Time.Format (formatTime, defaultTimeLocale)
+import Data.Time.LocalTime (getZonedTime)
+import System.Random (randomRIO)
+import qualified Data.Map as M
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
+import qualified Data.Text.Read as T
+
+import qualified Network.WebSockets as WS
+
+
+type ClientId = Text
+data Connection = Connection WS.Connection Int
+instance Eq Connection where
+    (Connection _ n1) == (Connection _ n2) = n1 == n2
+
+data Client = Client ClientId Connection
+clientId   (Client cid _)  = cid
+clientConn (Client _ conn) = conn
+withCid cid = filter ((==cid) . clientId)
+
+data ServerState = ServerState { clients :: [Client]
+                               , secrets :: Map ClientId Text
+                               , players :: [ClientId]
+                               , admins :: [ClientId]
+                               , nextConn :: Int
+                               , password :: Text
+                               , wall :: [(Int,Text)]
+                               , groups :: [Int] }
+
+idLength = 5
+secretLength = 10
+
+
+-- random utility functions (first generic, then codebase-specific)
+
+chomp :: Text -> Text
+chomp = T.reverse . T.dropWhile (=='\n') . T.reverse
+
+shuffle :: [a] -> IO [a]
+shuffle [] = pure []
+shuffle xs = do
+    idx <- randomRIO (0, length xs - 1)
+    let (left, (x:right)) = splitAt idx xs
+    (x:) <$> shuffle (left++right)
+
+hush :: Either a b -> Maybe b
+hush = either (const Nothing) Just
+
+between :: Ord a => a -> a -> a -> Bool
+between a b x = a <= x && x <= b
+
+chunksOf :: Int -> [a] -> [[a]]
+chunksOf _ [] = []
+chunksOf n xs = take n xs:chunksOf n (drop n xs)
+
+reqa :: ServerState -> ClientId -> IO ServerState -> IO ServerState
+reqa s@ServerState{admins} cid blk = if cid `elem` admins then blk else return s
+
+reqp :: ServerState -> ClientId -> IO ServerState -> IO ServerState
+reqp s@ServerState{players} cid blk = if cid `elem` players then blk else return s
+
+-- logging (not saved to a file, use tee)
+
+log :: String -> IO ()
+log s = getZonedTime >>= putStrLn . (++s) . formatTime defaultTimeLocale "[%F %T] "
+
+logC :: Connection -> String -> IO ()
+logC (Connection _ connid) s = log $ pad connid ++ ": " ++ s
+    where pad = reverse . take 5 . (++repeat '0') . reverse . show
+
+
+-- sending and receiving websocket messages
+
+sendWS :: Connection -> Text -> IO ()
+sendWS c@(Connection conn connid) t = do
+    logC c $ "SEND " ++ T.unpack t
+    WS.sendTextData conn t
+
+recvWS :: Connection -> IO Text
+recvWS c@(Connection conn connid) = do
+    t <- WS.receiveData conn
+    logC c $ "RECV " ++ T.unpack t
+    return t
+
+broadcast :: [Client] -> Text -> IO ()
+broadcast clients msg = forM_ clients $ flip sendWS msg . clientConn
+
+
+-- main logic
+
+app :: MVar ServerState -> WS.ServerApp
+app state pending = do
+    conn <- WS.acceptRequest pending
+    connid <- modifyMVar state $
+        \s@ServerState{nextConn} -> return (s{nextConn=nextConn+1}, nextConn)
+    WS.withPingThread conn 30 (pure ()) $ negotiate state (Connection conn connid)
+
+
+negotiate :: MVar ServerState -> Connection -> IO ()
+negotiate state conn = fmap (maybe () id) . runMaybeT $ do
+    lift $ logC conn "negotiate attempt"
+    msg <- lift $ recvWS conn
+    ServerState{secrets} <- lift $ readMVar state
+
+    lift . when (T.length msg /= idLength + secretLength ||
+                 T.any (not . liftM2 (&&) isUpper isAscii) msg) $ do
+        logC conn "misbehaving client??"
+        sendWS conn "e"
+        guard False
+
+    let (cid, sec) = T.splitAt idLength msg
+
+    lift . when (maybe False (/= sec) $ M.lookup cid secrets) $ do
+        logC conn "client id clash, trying again"
+        sendWS conn "r"
+        negotiate state conn
+        guard False
+
+    lift $ sendWS conn "g"
+    -- end of negotiation, client is good
+
+    let connect = do
+        logC conn "connected"
+        let c = Client cid conn
+        modifyMVar_ state $ \s@ServerState{clients,secrets,players,admins,wall,groups} -> do
+            -- catch up
+            when (cid `elem` admins)  $ sendWS conn $ encodeAdmin True
+            when (cid `elem` players) $ sendWS conn $ encodePlayer True
+            when (not $ null wall)    $ sendWS conn $ encodeWall wall
+            when (not $ null groups)  $ sendWS conn $ encodeGuess True groups
+            -- add client to list (and tell admins)
+            withClientsUpdate s { clients = c:clients
+                                , secrets = M.insert cid sec secrets }
+        -- main loop
+        forever $ do
+            msg <- recvWS conn
+            uncurry (play state c) $ T.splitAt 1 msg
+
+    let disconnect = do
+        logC conn "disconnected"
+        modifyMVar_ state $ \s@ServerState{clients,secrets} ->
+            withClientsUpdate s { clients = filter ((/= conn) . clientConn) clients }
+
+    lift $ connect `finally` disconnect
+
+
+play :: MVar ServerState -> Client -> Text -> Text -> IO ()
+
+-- requesting admin access
+play state (Client cid conn) "a" pwd =
+    modifyMVar_ state $ \s@ServerState{password,admins} -> do
+        let good = pwd == password
+        sendWS conn $ encodeAdmin good
+        if good then withClientsUpdate s { admins = cid:admins } else return s
+
+-- admin submitted a new wall
+play state (Client cid conn) "w" walldata =
+    modifyMVar_ state $ \s@ServerState{clients,wall,groups} -> reqa s cid $ do
+        -- jesus christ, what a line
+        new <- sequence $ liftM2 zip (shuffle [0..15]) . pure <$> parseWall walldata
+        -- tell everyone about it
+        sequence_ $ broadcast clients . encodeWall <$> new
+        return s { wall   = fromMaybe wall   new
+                 , groups = fromMaybe groups ([] <$ new)}
+    where parseWall "" = Just []
+          parseWall s = let cells = T.splitOn "\n" $ chomp s
+                         in cells <$ guard (length cells == 16)
+
+-- (un)make someone an admin
+-- ugly code duplication between this and the "P" case, but whatever
+play state (Client cid conn) "A" req =
+    modifyMVar_ state $ \s@ServerState{clients,admins} -> reqa s cid $ do
+        let (action,target) = T.splitAt 1 req
+            yes = action == "1"
+        -- we won't bother checking if target is real because we trust admins
+        broadcast (withCid target clients) $ encodeAdmin yes
+        withClientsUpdate s { admins = [target | yes] ++ filter (/=target) admins }
+
+-- (un)make someone a player
+play state (Client cid conn) "P" req =
+    modifyMVar_ state $ \s@ServerState{clients,players} -> reqa s cid $ do
+        let (action,target) = T.splitAt 1 req
+            yes = action == "1"
+        -- we won't bother checking if target is real because we trust admins
+        broadcast (withCid target clients) $ encodePlayer yes
+        withClientsUpdate s { players = [target | yes] ++ filter (/=target) players }
+
+-- player submitted a guess
+play state (Client cid conn) "g" guess =
+    modifyMVar_ state $ \s@ServerState{clients,groups} -> reqp s cid $
+        fromMaybe (return s) (makeGuess s <$> parseGuess s guess)
+    where parseGuess s@ServerState{groups} guess = do
+              xs <- sequence $ fmap fst . hush . T.decimal <$> T.splitOn "/" guess
+              guard $ all ($xs) [(==4) . length,
+                                 liftM2 (==) id nub,
+                                 all (liftM2 (&&) (between 0 15) (`notElem` groups))]
+              return xs
+          makeGuess s@ServerState{clients,groups,wall} guess =
+              if sort guess `elem` (map sort . chunksOf 4 $ fst <$> wall)
+                 then do
+                     let groups' = groups ++ guess
+                     broadcast clients $ encodeGuess True groups'
+                     return s { groups = groups' }
+                 else do
+                     broadcast clients $ encodeGuess False guess
+                     return s
+
+play _ (Client _ conn) _ _ = logC conn "misbehaving client??"
+
+
+encodeYN :: Char -> Bool -> Text
+encodeYN ch yes = T.pack $ ch:(if yes then "1" else "0")
+encodeAdmin :: Bool -> Text
+encodeAdmin = encodeYN 'a'
+encodePlayer :: Bool -> Text
+encodePlayer = encodeYN 'p'
+encodeWall :: [(Int,Text)] -> Text
+encodeWall = ('w' `T.cons`) . T.intercalate "\n" . map snd . sortOn fst
+encodeGuess :: Bool -> [Int] -> Text
+encodeGuess yes idxs = T.pack $ (if yes then 'g' else 'G'):intercalate "/" (show <$> idxs)
+encodeClients :: [Client] -> Map ClientId Text -> [ClientId] -> [ClientId] -> Text
+encodeClients clients secrets players admins =
+    ('c' `T.cons`) . T.intercalate "/" . map stringify . sortOn sortPred $ M.toList secrets
+    where sortPred (cid,sec) = fromMaybe (-1) $ findIndex ((==cid) . clientId) clients
+          stringify (cid,sec) = T.concat [fmtCount . length . withCid cid $ clients,
+                                          fmtYN "p" (cid `elem` players),
+                                          fmtYN "a" (cid `elem` admins), " ",
+                                          cid, " ",
+                                          T.take 5 sec, " ", T.drop 5 sec]
+          fmtCount n | n > 9 = "*"
+                     | n > 0 = T.pack $ show n
+                     | otherwise = " "
+          fmtYN s True  = s
+          fmtYN s False = " "
+
+withClientsUpdate :: ServerState -> IO ServerState
+withClientsUpdate s@ServerState{clients,secrets,players,admins} =
+    forM_ (filter ((`elem` admins) . clientId) clients)
+          (flip sendWS clientsEnc . clientConn) $> s
+    where clientsEnc = encodeClients clients secrets players admins
+
+
+main :: IO ()
+main = do
+    pwd <- chomp <$> T.readFile "pwd"
+    if pwd == "password"
+       then putStrLn "change the password ya dingus"
+       else do
+           state <- newMVar $ ServerState { clients = []
+                                          , secrets = M.empty
+                                          , players = []
+                                          , admins = []
+                                          , nextConn = 0
+                                          , password = pwd
+                                          , wall = []
+                                          , groups = [] }
+           log "starting server"
+           WS.runServer "127.0.0.1" 9255 $ app state
---
+++ netwall.js
@@ -0,0 +1,168 @@
+var $ = document.querySelector.bind(document),
+    $$ = document.querySelectorAll.bind(document);
+
+var NEGOTIATE = 0, PLAY = 1, ERROR = -1;
+
+var ws;
+
+window.addEventListener('load', () => {
+
+    var stage = NEGOTIATE, retrycount = 0,
+        cells = $$('div.cell'),
+        cltlist = $('#cltlist'),
+        wall = [], groups = [],
+        isPlayer = false;
+
+    ws = new WebSocket('ws://localhost:9255/');
+
+    var negotiate = regen => {
+        var key = (regen !== true && localStorage.getItem('key')) || genkey();
+        ws.send(key);
+    };
+    ws.onopen = negotiate;
+
+    ws.onmessage = e => {
+        var data = e.data.slice(1);
+        switch (stage) {
+
+            case NEGOTIATE:
+                switch (e.data) {
+                    case 'g':
+                        $('#key').textContent = localStorage.getItem('key').slice(0,5);
+                        stage = PLAY;
+                        break;
+                    case 'r':
+                        if (retrycount++ < 10) {
+                            negotiate(true);
+                            break;
+                        }
+                    default:
+                        stage = ERROR;
+                        alert(e.data === 'e' ? 'sorry, something weird happened' :
+                              e.data === 'r' ? 'sorry, something very weird happened' :
+                                               'sorry, something extremely weird happened');
+                }
+                break;
+
+            case PLAY:
+                switch (e.data[0]) {
+                    case 'a':
+                        if (data === '1') {
+                            console.log('you are an admin');
+                            $('#admin').style.display = 'block';
+                        } else {
+                            console.log('you are not an admin');
+                            $('#admin').style.display = 'none';
+                        }
+                        break;
+                    case 'p':
+                        $('#key').classList.toggle('player', isPlayer = data === '1');
+                        break;
+                    case 'c':
+                        while (cltlist.lastChild) cltlist.removeChild(cltlist.lastChild);
+                        data.split('/').forEach(clt => {
+                            var div = document.createElement('div');
+                            div.appendChild(document.createTextNode(clt + ' '));
+                            var btn1 = document.createElement('button'),
+                                btn2 = document.createElement('button');
+                            btn1.textContent = 'p';
+                            btn2.textContent = 'a';
+                            [btn1, btn2].forEach(btn => {
+                                btn.addEventListener('click', () => {
+                                    ws.send(toggleFlag(btn.textContent, clt));
+                                });
+                                div.appendChild(btn);
+                            });
+                            cltlist.appendChild(div);
+                        });
+                        break;
+                    case 'w':
+                        wall = data.split('\n');
+                        groups = [];
+                        render();
+                        break;
+                    case 'g':
+                        groups = data.split('/').map(x => +x);
+                        render();
+                        break;
+                    case 'G':
+                        flash(data.split('/').map(x => +x));
+                        break;
+                }
+                break;
+
+        }
+    };
+
+    ws.onclose = () => {
+        $('#discon').style.display = 'block';
+    };
+
+    var render = () => {
+        var rest = Array.from(Array(16).keys()).filter(x => groups.indexOf(x) === -1);
+        console.log(rest);
+        cells.forEach((cell, idx) => {
+            var realidx = groups[idx] !== undefined ? groups[idx] : rest[idx-groups.length];
+            cell.textContent = wall[realidx];
+            cell.dataset.idx = realidx;
+            cell.classList.toggle('solved', idx < groups.length);
+        });
+    };
+
+    var flash = idxs => {
+        cells.forEach(cell => {
+            if (idxs.indexOf(+cell.dataset.idx) !== -1) cell.classList.add('wrong');
+        });
+        setTimeout(() => {
+            cells.forEach(cell => cell.classList.remove('wrong'));
+        }, 200);
+    };
+
+    var actions = {
+        set: () => { ws.send('w' + $('#settxt').value); },
+        clr: () => { ws.send('w'); }
+    };
+
+    $$('#admin > span').forEach(link => {
+        var modal = $('#' + link.id + 'm'), tmp;
+        link.addEventListener('click', () => {
+            modal.style.display = 'flex';
+        });
+        (tmp = modal.querySelector('.confirm')) && tmp.addEventListener('click', () => {
+            actions[link.id]();
+            modal.style.display = 'none';
+        });
+        modal.querySelector('.dismiss').addEventListener('click', () => {
+            modal.style.display = 'none';
+        });
+    });
+
+    cells.forEach((cell, idx) => {
+        cell.addEventListener('click', () => {
+            if (isPlayer && wall.length && idx >= groups.length) {
+                cell.classList.toggle('selected');
+                var guesses = Array.from($$('.selected'));
+                if (guesses.length === 4) {
+                    guesses.forEach(g => g.classList.remove('selected'));
+                    ws.send('g' + guesses.map(g => g.dataset.idx).join('/'));
+                }
+            }
+        });
+    });
+
+});
+
+var alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+function genkey() {
+    var key = Array(15).fill().map(() => alphabet[Math.random() * alphabet.length | 0]).join('');
+    localStorage.setItem('key', key);
+    return key;
+}
+
+function admin(pwd) {
+    ws.send('a' + pwd);
+}
+
+function toggleFlag(flag, clt) {
+    return flag.toUpperCase() + (clt.indexOf(flag) === -1 ? '1' : '0') + clt.substr(4,5);
+}
