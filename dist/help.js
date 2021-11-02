b9a0ec6afd763780aa21d9e9d340d54bcd7e7d37
Andy Tockman <andy@tck.mn>
2021-11-02 17:23:14 -0400

    even more help/about

--- dist/help.js
+++ dist/help.js
@@ -1,3 +1,41 @@
+var gallery = [
+    {
+        img: 'cset1.jpg',
+        caption: 'The very first printed deck. This was an early prototype of C53T with different card styles; we also originally played in "easy mode" with the cards sorted by one of the hexagons.',
+        date: '2021-09-18'
+    },
+    {
+        img: 'firstcset.jpg',
+        caption: 'One of the first C53Ts ever found.',
+        date: '2021-09-18'
+    },
+    {
+        img: 'voting1.jpg',
+        caption: 'An early snapshot of the table of proposals for set variants, which has since gone through many additions and changes.',
+        date: '2021-09-22'
+    },
+    {
+        img: 'houseofcards.jpg',
+        caption: 'A house of cards. (Can you find a set in each "room"?)',
+        date: '2021-09-30'
+    },
+    {
+        img: 'zendo.jpg',
+        caption: 'I wrote these examples on the whiteboard with no other explanation, and posed it as a puzzle for everyone else to figure out.',
+        date: '2021-10-01'
+    },
+    {
+        img: 'speedrun.jpg',
+        caption: 'The result of my speedrun through a full C53T deck, clocking in at 51:28.90. The fastest single set was 21.36, including grabbing the set and redealing.',
+        date: '2021-10-05'
+    },
+    {
+        img: 'behindthescenes.jpg',
+        caption: 'Behind the scenes of the process of making tsetse, checking to make sure the cards match the real-life ones.',
+        date: '2021-11-02'
+    }
+];
+
 function card(ex, x) {
     var c = m.dom.el('div', { class: 'helpcard' });
     c.appendChild(x);
@@ -30,14 +68,35 @@ window.addEventListener('load', () => {
 
     var ex = document.getElementById('A5SETex1');
     card(ex, m.A5SET.render([3,1,4,0,2]));
-    // card(ex, m.A5SET.render([2,3,0,1,4]));
     card(ex, m.A5SET.render([4,2,1,3,0]));
-    // card(ex, m.A5SET.render([0,4,3,2,1]));
     card(ex, m.A5SET.render([1,0,2,4,3]));
 
     var ex = document.getElementById('A5SETex2');
     card(ex, m.A5SET.render([3,2,0,4,1]));
     card(ex, m.A5SET.render([3,1,0,2,4]));
     card(ex, m.A5SET.render([3,4,0,1,2]));
+
+    var gprev = document.getElementById('gprev'),
+        gnext = document.getElementById('gnext'),
+        gdate = document.getElementById('gdate'),
+        gcap = document.getElementById('gcap'),
+        gimg = document.getElementById('gimg'),
+        gcur = 0,
+        ggo = diff => {
+            var url = '/img/gallery/'+gallery[gcur].img,
+                a = m.dom.el('a', { href: url, target: '_blank' });
+            gcur = (gcur + diff + gallery.length) % gallery.length;
+            gcap.innerText = gallery[gcur].caption;
+            gdate.innerText = gallery[gcur].date;
+            m.dom.clr(gimg);
+            a.appendChild(m.dom.el('img', {
+                src: '/img/gallery/'+gallery[gcur].img,
+                style: 'max-height: 400px'
+            }));
+            gimg.appendChild(a);
+        };
+    gprev.addEventListener('click', () => ggo(-1));
+    gnext.addEventListener('click', () => ggo(1));
+    ggo(0);
 
 });
