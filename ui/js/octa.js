4365342b47facf4db4adc52c80905a25fb434c5a
Andy Tockman <andy@tck.mn>
2021-11-02 02:16:28 -0400

    finish octa

--- ui/js/octa.js
+++ ui/js/octa.js
@@ -3,7 +3,8 @@ m.OCTA = (function() {
     return {
 
         render: function(card) {
-            var spirat = 0.3,
+            var pad = 0.2,
+                spirat = 0.3,
                 op = r => i => [Math.sin(2*Math.PI/3*i)*r, Math.cos(2*Math.PI/3*i)*r],
                 oout = op(1), oin = op(spirat),
                 ocolor = [
@@ -11,11 +12,41 @@ m.OCTA = (function() {
                     '#ff9600',
                     '#ff00c8',
                     '#00be96'
+                ],
+                cfaces = [
+                    [1,2,3,0],
+                    [3,2,0,1],
+                    [1,3,0,2]
+                ],
+                ccolor = [
+                    '#f00',
+                    '#0f0',
+                    '#00f'
                 ];
 
-            var draw = m.draw.create('-1 -1 2 2');
+            var draw = m.draw.create(`-${1+pad} -${2+1.5*pad} ${2+2*pad} ${4+3*pad}`);
+
+            // cube
+            draw.group({ transform: `translate(0,-${1+pad/2})` });
+            var idx = card[0].indexOf(0);
+            cfaces.forEach((c, i) => {
+                var quad = [[0,0], oout(i-0.5), oout(i), oout(i+0.5)],
+                    a = card[0][c[idx]],
+                    b = card[0][c[c[idx]]];
+                draw.poly(quad, {
+                    fill: ccolor[b-1], fancy: 1
+                });
+                if (((b-a+3)%3 === 1) ^ card[1]) {
+                    draw.poly(quad, {
+                        fill: '#fff',
+                        transform: `scale(0.6) translate(${op(0.35)(i).join(',')})`
+                    });
+                }
+            });
+            draw.fancy(0.08, 0.05);
 
             // octahedron
+            draw.group({ transform: `translate(0,${1+pad/2})` });
             m.util.range(3, i => {
                 draw.poly([oout(i), oout(i+0.5), oout(i+1)], {
                     fill: ocolor[card[0][i]], fancy: 1
@@ -24,9 +55,7 @@ m.OCTA = (function() {
             draw.poly(m.util.range(3, i => oout(i)), {
                 fill: ocolor[card[0][3]], fancy: 1
             });
-            draw.fancy(0.16, 0.1);
-
-            // parity swirl
+            draw.fancy(0.08, 0.05);
             m.util.range(3, i => {
                 draw.el('path', {
                     d: `M 0 0 A ${spirat/2} ${spirat/2} 0 0 ${card[1] ? 0 : 1} ${oin(i).join(' ')}`,
