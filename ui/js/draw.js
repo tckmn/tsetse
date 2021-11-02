86f64981a64c958504abace65b8ed3a60d6e681c
Andy Tockman <andy@tck.mn>
2021-11-01 20:46:38 -0400

    add parity swirl to octa

--- ui/js/draw.js
+++ ui/js/draw.js
@@ -3,16 +3,19 @@ m.draw = (function() {
     return {
 
         create: viewBox => ({
+            el: function(name, props) {
+                this.svg.appendChild(m.dom.svgel(name, props));
+            },
             polys: [],
             poly: function(pts, props) {
                 if (props && props.fancy) {
                     this.polys.push(pts);
                     delete props.fancy;
                 }
-                this.svg.appendChild(m.dom.svgel('path', {
+                this.el('path', {
                     d: 'M ' + pts.map(a => a.join(' ')).join(' L ') + ' Z',
                     ...props
-                }));
+                });
             },
             fancy: function(outer, inner) {
                 this.polys.forEach(p => {
--- ui/js/octa.js
+++ ui/js/octa.js
@@ -3,7 +3,9 @@ m.OCTA = (function() {
     return {
 
         render: function(card) {
-            var op = i => [Math.sin(2*Math.PI/3*i), Math.cos(2*Math.PI/3*i)],
+            var spirat = 0.3,
+                op = r => i => [Math.sin(2*Math.PI/3*i)*r, Math.cos(2*Math.PI/3*i)*r],
+                oout = op(1), oin = op(spirat),
                 ocolor = [
                     '#9100d7',
                     '#ff9600',
@@ -13,16 +15,25 @@ m.OCTA = (function() {
 
             var draw = m.draw.create('-1 -1 2 2');
 
+            // octahedron
             m.util.range(3, i => {
-                draw.poly([op(i), op(i+0.5), op(i+1)], {
+                draw.poly([oout(i), oout(i+0.5), oout(i+1)], {
                     fill: ocolor[card[0][i]], fancy: 1
                 });
             });
-            draw.poly(m.util.range(3, i => op(i)), {
+            draw.poly(m.util.range(3, i => oout(i)), {
                 fill: ocolor[card[0][3]], fancy: 1
             });
-
             draw.fancy(0.16, 0.1);
+
+            // parity swirl
+            m.util.range(3, i => {
+                draw.el('path', {
+                    d: `M 0 0 A ${spirat/2} ${spirat/2} 0 0 ${card[1] ? 0 : 1} ${oin(i).join(' ')}`,
+                    fill: 'none', stroke: card[1] ? '#000' : '#fff', strokeWidth: 0.05,
+                    strokeLinejoin: 'round', strokeLinecap: 'round'
+                });
+            });
 
             return draw.svg;
         },
