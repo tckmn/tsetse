50a866c187f79a58258d0bf4cbef767816a7b92c
Andy Tockman <andy@tck.mn>
2021-11-03 14:03:45 -0400

    some modals
    name change and admin

--- ui/js/e.js
+++ ui/js/e.js
@@ -3,7 +3,12 @@ m.e = (function() {
     return {
 
         _onload: function() {
-            'wrap wallwrap wall gamelist name discon sbmain sbconf sbtoggle lobby helplink claim pluscard'.split(' ').forEach(id => this[id] = document.getElementById(id));
+            'wrap wallwrap wall gamelist name discon showadmin sbmain sbconf sbtoggle lobby helplink claim pluscard'.split(' ').forEach(id => this[id] = document.getElementById(id));
+
+            if (localStorage.getItem('password')) this.wrap.classList.add('hasadmin');
+
+            this.name.addEventListener('click', () => m.modal.show('namechange'));
+            this.showadmin.addEventListener('click', () => m.modal.show('admin'));
 
             this.lobby.addEventListener('click', e => {
                 e.preventDefault();
---
+++ ui/js/modal.js
@@ -0,0 +1,47 @@
+m.modal = (function() {
+
+    var pwd = { password: localStorage.getItem('password') };
+
+    var v = {};
+    'newname'.split(' ').forEach(x => { v[x] = () => document.getElementById(x).value; });
+
+    var actions = {
+
+        namechange: () => {
+            m.net.send('Uname', { uname: v.newname() });
+            m.modal.close();
+        },
+
+        savestate: () => {
+            m.net.send('SaveState', pwd);
+        }
+
+    };
+
+    return {
+
+        active: undefined,
+
+        show: function(id) {
+            if (this.active) return;
+            this.active = document.getElementById('modal_'+id);
+            this.active.style.display = 'flex';
+        },
+
+        close: function() {
+            if (this.active) this.active.style.display = 'none';
+            this.active = undefined;
+        },
+
+        _onload: function() {
+            Array.from(document.getElementsByClassName('dismiss')).forEach(e => {
+                e.addEventListener('click', this.close.bind(this));
+            });
+            for (a in actions) {
+                document.getElementById(a).addEventListener('click', actions[a]);
+            }
+        }
+
+    };
+
+})();
